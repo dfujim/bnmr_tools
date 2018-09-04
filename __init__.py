e5e1bbf85787ceeed9ecb712897085a8569666ce
Derek Fujimoto <fujimoto@phas.ubc.ca>
2018-09-04 14:51:26 -0700

    Now a proper module

---
+++ __init__.py
@@ -0,0 +1,10 @@
+__author__ = "Derek Fujimoto"
+__version__ = "0.0.0"
+__all__ = ['beta_averaging','parse_bnmroffice','file_checkup']
+
+from bnmr_tools.beta_averaging import t1avg
+from bnmr_tools.beta_averaging import dt1avg
+from bnmr_tools.parse_bnmroffice_fit import parse_bnmroffice_fit
+from bnmr_tools.file_checkup import file_checkup
+
+
