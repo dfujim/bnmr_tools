c51ab01ab143770fb58636b053bb12739824f328
Derek Fujimoto <fujimoto@phas.ubc.ca>
2018-11-19 12:38:50 -0800

    Updated __init__ with depth fit

--- __init__.py
+++ __init__.py
@@ -1,10 +1,11 @@
 __author__ = "Derek Fujimoto"
 __version__ = "0.0.0"
-__all__ = ['beta_averaging','parse_bnmroffice','file_checkup']
+__all__ = ['beta_averaging','parse_bnmroffice','file_checkup','srim','depth_fit']
 
 from bnmr_tools.beta_averaging import t1avg
 from bnmr_tools.beta_averaging import dt1avg
 from bnmr_tools.parse_bnmroffice_fit import parse_bnmroffice_fit
 from bnmr_tools.file_checkup import file_checkup
-
+from bnmr_tools.srim import srim
+from bnmr_tools.depth_fit import depth_fit
 
