a1676a2f374c9686a4fdbd8b7b7f095a632fda29
Derek Fujimoto <fujimoto@phas.ubc.ca>
2018-11-28 19:46:53 -0800

    beta_avg added correlated errors

--- beta_averaging.py
+++ beta_averaging.py
@@ -10,9 +10,23 @@ def t1avg(T1, beta) :
     return T1*gamma(1./beta)/beta
 
 # Calculate uncertainty in <T_1> (i.e., beta-averaged relaxation time)
-def dt1avg(T1, dT1, beta, dbeta) :
+def dt1avg(T1, dT1, beta, dbeta, cov=0) :
+    """cov = covarince between T1 and beta"""
+    
     betai = 1./beta
     pd_T1 = gamma(1./beta)/beta
-    #~ pd_beta = T1*( beta + polygamma(0, 1./beta) )/( beta*gamma(1./beta) )
-    pd_beta = T1*gamma(betai)*(1+betai*polygamma(0,betai))*square(betai)
-    return sqrt( (pd_T1*dT1)**2 + (pd_beta*dbeta)**2 )
+    pd_beta = -T1*gamma(betai)*(1+betai*polygamma(0,betai))*square(betai)
+    return sqrt( (pd_T1*dT1)**2 + (pd_beta*dbeta)**2 + 2*cov*pd_T1*pd_beta) 
+
+# Calculate uncertainty in <T_1> (i.e., beta-averaged relaxation time), from 1/T1
+def dt1avg_fromL(lambda1, dlambda1, beta, dbeta, cov=0) :
+    """cov = covarince between 1/T1 and beta"""
+    
+    # get covariance between T1 and beta, from 1/T1 and beta
+    cov = -cov/square(lambda1)
+    
+    # get T1
+    T1 = 1/lambda1
+    dT1 = dlambda1/square(lambda1)
+        
+    return dt1avg(T1, dT1, beta, dbeta, cov) 
